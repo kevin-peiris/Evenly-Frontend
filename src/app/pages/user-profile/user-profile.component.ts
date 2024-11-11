48b102929e9d0bde9373cf0bbd00fd104267d118
kevin-peiris <kevin.2007.01.22@gmail.com>
2024-11-11 14:46:41 +0530

    User Profile page done

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,14 +1,53 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { NavBarUserComponent } from '../../common/nav-bar-user/nav-bar-user.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+
+  mainTitle = 'Less stress when sharing expenses on trips.';
+  mainDescription = 'Keep track of your shared expenses and balances with housemates, trips, groups, friends, and family.';
+  mainImage = 'house.png';
+  mainColor = 'text-primary';
+
+
+  updateContent(newTitle: string, newDescription: string, newImage: string) {
+    this.mainTitle = `Less stress when sharing expenses ${newTitle}`;
+    this.mainDescription = newDescription;
+    this.mainImage = newImage;
+  }
+
+  public user: any = {
+    name: "",
+    email: "",
+    password: ""
+  };
+
+  ngOnInit(): void {
+    this.userService.currentUser.subscribe(user => {
+      this.user = user;
+    });
+  }
+
+  constructor(private http: HttpClient, private userService: UserService, private router: Router) { }
+
+  updateDetails() {
+    this.http.put("http://localhost:8080/users/update", this.user, { responseType: 'text' })
+        .subscribe((data) => {
+          this.userService.setUser(this.user);
+          this.router.navigate(['/user-main/user-dashboard']);
+        });
+  }
 
 }
